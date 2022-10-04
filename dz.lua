965ab95a895e7104ce25622198009afbca7bca6d
vadzikpawer <mromanko@list.ru>
2022-10-04 23:05:22 +0300

    init commit

--- dz.lua
+++ dz.lua
@@ -4,12 +4,20 @@ function Func(arr)
     local index = 0
     local j = 0
     for x in pairs(arr) do
+        if type(x) ~= "table" then
+            goto continue
+        end
         j = j + 1
         sum1 = sum1 + arr[x]['p']
         if sum1 * 2 >= half then
-            index = j + (math.abs(2 * sum1 - half) < math.abs(2 * (sum1 - arr[x]['p']) - half))
+            local add = 0
+            if math.abs(2 * sum1 - half) < math.abs(2 * (sum1 - arr[x]['p']) - half) then
+                add = 1
+            end
+            index = j + add
             break
-    end
+        end
+        ::continue::
     end
 
     local arr0, arr1 = {}, {}
@@ -18,17 +26,18 @@ function Func(arr)
         if i >= index then
             break
         end
-        arr[x]['code'] = arr[x]['code'] + '0'
+        arr[x]['code'] = arr[x]['code'] .. '0'
         arr0 = arr[x]
         i = i + 1
     end
     for x in pairs(arr) do
-        if i < index then
-            ::continue::
+        if i < index | type(x) ~= "table" then
+            goto continue
         end
-        i = i + 1
-        arr[x]['code'] = arr[x]['code'] + '1'
+        arr[x]['code'] = arr[x]['code'] .. '1'
         arr1[x] = arr[x]
+        ::continue::
+        i = i + 1
     end
     if Len(arr1) == 1 then 
         table.insert(Code, arr1)
@@ -45,8 +54,13 @@ end
 
 function Sum(arr)
     local out = 0
-    for x in pairs(arr) do
+    for x,v in pairs(arr) do
+        if type(x) ~= "table" then
+            goto continue
+        end
+        print(x)
         out = out + arr[x]['p']
+        ::continue::
     end
     return out
 end
@@ -62,12 +76,11 @@ os.execute("chcp 65001")
 local alp = {}
 alp['а'] = {}
 alp['а']['p'] = 0.1
-alp['а']['Code'] = 1111
-print(alp['а']['p'], alp['а']['Code'], Len(alp))
-
-for x in pairs(alp) do
-    print(x,alp[x]['p'])
-end
-print(alp['a'])
+alp['а']['code'] = ''
 
 Code = {}
+
+Func(alp)
+for x in pairs(alp) do
+    print(x, alp[x]['p'], alp[x]['code'])
+end
