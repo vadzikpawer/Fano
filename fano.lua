1988c80df73caf8ae38561990acfcc2eafd8b5e4
vadzikpawer <mromanko@list.ru>
2022-10-08 17:07:12 +0300

    add new input method

--- fano.lua
+++ fano.lua
@@ -10,7 +10,7 @@ function Func(arr)
             if math.abs(2 * sum1 - half) < math.abs(2 * (sum1 - arr[x][2]) - half) and Len(arr) ~= 2 then
                 add = 1
             end
-            if Len(arr) == 2 then
+            if Len(arr) == 2 or Len(arr) == 3 then
                 index = 2
             else
                 index = x + add
@@ -83,40 +83,99 @@ function PrintArr(arr)
     print('\n')
 end
 
+function StrToTb(str)
+    local out = {}
+    str = Clean(str)
+    print(str)
+    local ln = 0
+    for i = 1, #str do
+        ln = ln + 1
+        local c = str:sub(i,i)
+        local index = Is(out, c)
+        if index ~= -1 then
+            out[index][2] = out[index][2] + 1
+        else
+            out[Len(out)+1] = {}
+            out[Len(out)][1] = c
+            out[Len(out)][2] = 1
+            out[Len(out)][3] = ''
+        end
+    end
+    for i=1, #out do
+        out[i][2] = out[i][2]/ln
+    end
+    return out
+end
+
+function Is(arr, symb)
+    local j = -1
+    if Len(arr) == 0 then
+        return -1
+    end
+    for i=1, #arr do
+        if arr[i][1] == symb then
+            return i
+        end
+    end
+    return j
+end
+
+function Clean(str)
+    for i=1,#str do
+        local c = str:sub(i,i)
+        if c == '.' or c == ',' or c == '?' or c == '/' then
+            str = str:sub(1,i-1) .. str:sub(i+1)
+        end
+    end
+    return str
+end
+
+
 local alp = {}
 Code = {}
 
 os.execute("chcp 1251")
-io.write('Введите количество символов: ')
+print("1. Ввод строки")
+print("2. Посимвольный ввод")
+io.write('Выберите режим работы: ')
 local n = io.read('n')
-for j=1,n do
-    io.write('Введите символ: ')
+if n == 2 then
+    io.write('Введите количество символов: ')
+    n = io.read('n')
+    for j=1,n do
+        io.write('Введите символ: ')
+        local _ = io.read()
+        local a = io.read("l")
+        io.write('Введите вероятность символа: ')
+        local p = io.read('n')
+        while  p <= 0 do
+            io.write("Вероятность символа не может быть отрицательной или равна 0, Повторите ввод: ")
+            p = io.read('n')
+        end
+        while p > 1 do
+            io.write("Вероятность не может быть больше 1, повторите ввод: ")
+            p = io.read('n')
+        end
+        alp[j] = {}
+        alp[j][1] = a
+        alp[j][2] = p
+        alp[j][3] = ''
+    end
+
+    if Sum(alp) > 1 then
+        print('Суммарная вероятность не может быть больше 1')
+        return
+    end
+elseif n == 1 then
+    io.write('Введите строку: ')
     local _ = io.read()
     local a = io.read("l")
-    io.write('Введите вероятность символа: ')
-    local p = io.read('n')
-    while  p <= 0 do
-        io.write("Вероятность символа не может быть отрицательной или равна 0, Повторите ввод: ")
-        p = io.read('n')
-    end
-    while p > 1 do
-        io.write("Вероятность не может быть больше 1, повторите ввод: ")
-        p = io.read('n')
-    end
-    alp[j] = {}
-    alp[j][1] = a
-    alp[j][2] = p
-    alp[j][3] = ''
+    alp = StrToTb(a)
 end
 
-if Sum(alp) > 1 then
-    print('Суммарная вероятность не может быть больше 1')
-    return
-end
 
 table.sort(alp, Compare)
 
 Func(alp)
 
-table.sort(Code, Compare)
 PrintArr(Code)
